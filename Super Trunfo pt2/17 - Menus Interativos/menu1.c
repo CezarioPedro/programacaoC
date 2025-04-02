e321e1a60f386cb32e44cc2e5dc5298bbf2ff39a
Pedro (estudos) <cezariopedro16adm@gmail.com>
2025-04-01 23:34:05 -0300

    Puxei para a maquina principal

--- Super Trunfo pt2/17 - Menus Interativos/menu1.c
+++ Super Trunfo pt2/17 - Menus Interativos/menu1.c
@@ -3,27 +3,27 @@
 int main(){
     int opcao;
 
-    printf("Menu Principal\n")
-    printf("1. Iniciar Jogo\n")
-    printf("2. Ver Regras")
-    printf("3. Sair")
+    printf("Menu Principal\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Ver Regras");
+    printf("3. Sair");
 
     switch (opcao){
     case 1:
         printf("Iniciando o jogo...\n");
     break;
-    case 1:
+    case 2:
     printf("Regras do jogo:\n");
-    printf("Regra número 1.\n")
-    printf("Regra número 2.\n")
-    printf("Regra número 3.\n")
+    printf("Regra número 1.\n");
+    printf("Regra número 2.\n");
+    printf("Regra número 3.\n");
     break;
     case 3:
     printf("Fechando o jogo...\n");
     break;
     
     default:
-        printf("Opção inválida.")
+        printf("Opção inválida.");
     break;
     }
 }
